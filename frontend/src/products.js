457eaf05c8e97953d57d6e9e28a346a2e7bbdd1e
Allahnana <usmanallahnana@gmail.com>
2023-06-29 17:06:08 +0100

    ...

---
+++ frontend/src/products.js
@@ -0,0 +1,86 @@
+const product = [
+    {
+        _id: '1',
+        name: 'Airpods Wireless Bluetooth Headphones',
+        image: '/images/airpods.jpg',
+        description: 
+        'Bluetooth Technologies lets you connect with compatible devices wirelessly High-quality AAC audio offers immersive quality listening for experience Built-in microphone allows you to call while working.',
+        brand: 'Apple',
+        category: 'electronics',
+        price: 100.10,
+        countInStock: 3,
+        rating: 4.5,
+        numReviews: 1,
+
+    },
+    {
+        _id: '2',
+        name: 'Iphone 11 Pro 256GB Memory',
+        image: '/images/phone.jpg',
+        description: 
+        'Introducing Iphone 11 Pro. A Transfomative tripple camera system that add tons capability without complexity. An unprecedented in battery life.',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 150.10,
+        countInStock: 3,
+        rating: 4.5,
+        numReviews: 3,
+
+    },
+    {
+        _id: '3',
+        name: 'Cannon EOS 80D DSLR Camera',
+        image: '/images/camera.jpg',
+        description: 
+        'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itselfs using a pair of robust focusing systems and an intuitive design.',
+        brand: 'Cannon',
+        category: 'Electronics',
+        price: 892.10,
+        countInStock: 5,
+        rating: 2.5,
+        numReviews: 7,
+
+    },
+    {
+        _id: '4',
+        name: 'Sony Playstation 4 Pro White Version ',
+        image: '/images/playstation.jpg',
+        description: 
+        'The ultimate home entertainment center starts with Playstation. Whether you are into gaming, HD movies, television, music. .',
+        brand: 'Sony',
+        category: 'Electronics',
+        price: 150.10,
+        countInStock: 13,
+        rating: 9.5,
+        numReviews: 7,
+
+    },
+    {
+        _id: '5',
+        name: 'Logitech G-Series Gaming Mouse',
+        image: '/images/mouse.jpg',
+        description: 
+        'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for smooth playing experience.',
+        brand: 'Logitech',
+        category: 'Electronics',
+        price: 250.10,
+        countInStock: 2,
+        rating: 4.5,
+        numReviews: 3,
+
+    },
+    {
+        _id: '6',
+        name: 'Amazon Echo Dot 3rd Generation',
+        image: '/images/alexa.jpg',
+        description: 
+        'Meet Echo Dot - Our most popular smart speaker with a faric design. It is our most compact smart speaker that fits perfectly into small space.',
+        brand: 'Amazon',
+        category: 'Electronics',
+        price: 50.10,
+        countInStock: 3,
+        rating: 4.5,
+        numReviews: 1,
+
+    },
+]
